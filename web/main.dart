385a3bb86ebf56a0b45265a0f1b40b14bcdde4f8
Rado Kirov <radokirov@google.com>
2015-01-15 16:58:25 -0800

    first commit. adds angular.dart 2.0 hello world app.
    1) clone angular/angular
    2) run `gulp build`
    3) Rename $HOME in pubspec.yaml to home directory, because 'pub' doesn't
    expand '~'
    4) pub serve

---
+++ web/main.dart
@@ -0,0 +1,34 @@
+// will become angular2.
+import 'package:core/core.dart' show bootstrap, Component, Decorator, TemplateConfig, NgElement;
+
+// for demos. remove for produciton, use transformer.
+import 'package:reflection/reflection.dart' show reflector;
+import 'package:reflection/reflection_capabilities.dart' show ReflectionCapabilities;
+
+@Component(selector: 'hello-app', componentServices: const[GreetingService], template: const TemplateConfig(inline: '''<div class=\"greeting\">{{greeting}} <span red>world</span>!</div>
+             <button class=\"changeButton\" (click)=\"changeGreeting()\">change greeting</button>''', directives: const[RedDec]))
+class HelloCmp {
+String greeting;
+HelloCmp(GreetingService service) {
+  this.greeting = service.greeting;
+}
+changeGreeting() {
+  this.greeting = 'howdy';
+}
+}
+@Decorator(selector: '[red]')
+class RedDec {
+RedDec(NgElement el) {
+  el.domElement.style.color = 'red';
+}
+}
+class GreetingService {
+String greeting;
+GreetingService() {
+  this.greeting = 'hello';
+}
+}
+main() {
+  reflector.reflectionCapabilities = new ReflectionCapabilities();
+  bootstrap(HelloCmp);
+}
