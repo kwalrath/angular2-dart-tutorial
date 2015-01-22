af8486d69c072c75a883b62cd6cf4b7875134de9
Kathy Walrath <kathyw@google.com>
2015-01-22 15:56:44 -0800

    formatted with dartformat

--- Chapter_01/web/main.dart
+++ Chapter_01/web/main.dart
@@ -5,28 +5,32 @@ import 'package:core/core.dart' show bootstrap, Component, Decorator, TemplateCo
 import 'package:reflection/reflection.dart' show reflector;
 import 'package:reflection/reflection_capabilities.dart' show ReflectionCapabilities;
 
-@Component(selector: 'hello-app', componentServices: const[GreetingService], template: const TemplateConfig(inline: '''<div class=\"greeting\">{{greeting}} <span red>world</span>!</div>
-             <button class=\"changeButton\" (click)=\"changeGreeting()\">change greeting</button>''', directives: const[RedDec]))
+@Component(selector: 'hello-app', componentServices: const [
+  GreetingService
+], template: const TemplateConfig(
+    inline: '''<div class=\"greeting\">{{greeting}} <span red>world</span>!</div>
+             <button class=\"changeButton\" (click)=\"changeGreeting()\">change greeting</button>''',
+    directives: const [RedDec]))
 class HelloCmp {
-String greeting;
-HelloCmp(GreetingService service) {
-  this.greeting = service.greeting;
-}
-changeGreeting() {
-  this.greeting = 'howdy';
-}
+  String greeting;
+  HelloCmp(GreetingService service) {
+    this.greeting = service.greeting;
+  }
+  changeGreeting() {
+    this.greeting = 'howdy';
+  }
 }
 @Decorator(selector: '[red]')
 class RedDec {
-RedDec(NgElement el) {
-  el.domElement.style.color = 'red';
-}
+  RedDec(NgElement el) {
+    el.domElement.style.color = 'red';
+  }
 }
 class GreetingService {
-String greeting;
-GreetingService() {
-  this.greeting = 'hello';
-}
+  String greeting;
+  GreetingService() {
+    this.greeting = 'hello';
+  }
 }
 main() {
   reflector.reflectionCapabilities = new ReflectionCapabilities();
